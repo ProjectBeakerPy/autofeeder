bd165b9ea4dbc0b7e58782d1ccea49f8a0ab2621
sircaptainmitch <hamann.mitchell@gmail.com>
2020-02-01 15:49:57 -0600

    testing loop change

--- catguy.py
+++ catguy.py
@@ -9,7 +9,7 @@ def feed():
     # let the GPIO library know where we've connected our servo to the Pi
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(12, GPIO.OUT)
-    sleeptime = float(sys.argv[1])
+    attempts = float(sys.argv[1])
 
     try:
         servo = GPIO.PWM(12, 50)
@@ -17,12 +17,11 @@ def feed():
 
         # spin left, right, then left again rather than in a continuous circle
         # to prevent the food from jamming the servo
-        for index in range(0, 4):
+        for index in range(0, attempts):
             dutyCycle = 2.5 if (index % 2 == 0) else 12.5
             servo.ChangeDutyCycle(dutyCycle)
             # adjust the sleep time to have the servo spin longer or shorter in that direction
-            print(sleeptime)
-            time.sleep(sleeptime)
+            time.sleep(1)
     finally:
         # always cleanup after ourselves
         servo.stop()
